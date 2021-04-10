27439befb0d591faba4d6ebee51eb795cd8e7a3c
ZhangYuanSheng <2862297923@qq.com>
2021-04-10 23:53:19 +0800

    1.3.1-beta

--- src/main/java/com/github/restful/tool/service/Notify.java
+++ src/main/java/com/github/restful/tool/service/Notify.java
@@ -2,6 +2,7 @@ package com.github.restful.tool.service;
 
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
+import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
@@ -54,4 +55,56 @@ public interface Notify {
      * @return Notification
      */
     Notification notify(@NotNull String content, @NotNull NotificationType type);
+
+    /**
+     * warning-notification
+     *
+     * @param content 内容
+     * @param actions actions
+     * @return Notification
+     */
+    default Notification warning(@NotNull String content, AnAction... actions) {
+        Notification notification = warning(content);
+        if (actions != null && actions.length > 0) {
+            for (AnAction action : actions) {
+                notification.addAction(action);
+            }
+        }
+        return notification;
+    }
+
+    /**
+     * error-notification
+     *
+     * @param content 内容
+     * @param actions actions
+     * @return Notification
+     */
+    default Notification error(@NotNull String content, AnAction... actions) {
+        Notification notification = error(content);
+        if (actions != null && actions.length > 0) {
+            for (AnAction action : actions) {
+                notification.addAction(action);
+            }
+        }
+        return notification;
+    }
+
+    /**
+     * notify
+     *
+     * @param content 内容
+     * @param type    方式
+     * @param actions actions
+     * @return Notification
+     */
+    default Notification notify(@NotNull String content, @NotNull NotificationType type, AnAction... actions) {
+        Notification notification = notify(content, type);
+        if (actions != null && actions.length > 0) {
+            for (AnAction action : actions) {
+                notification.addAction(action);
+            }
+        }
+        return notification;
+    }
 }
